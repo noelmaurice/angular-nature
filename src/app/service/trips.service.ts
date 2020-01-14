4ec33744586df6613a63e731d48d14e083cd3924
noelmaurice <noelmaurice4@gmail.com>
2020-01-14 10:18:43 +0100

    Save

--- src/app/service/trips.service.ts
+++ src/app/service/trips.service.ts
@@ -4,18 +4,18 @@ import { Observable } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
 import { Trip } from '../model/trip';
 import { ErrorService } from './error.service';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
 	providedIn: 'root'
 })
 export class TripsService {
 
-	private tripsUrl = 'api/trips'; // 'http://localhost:9090/trip/all';
+	private tripsUrl = environment.server.serverURL;
 
 	constructor(
 		private http: HttpClient,
 		private errorService: ErrorService) { }
-
 
 	public getTrips(): Observable<Trip[]> {
 		return this.http.get<Trip[]>(this.tripsUrl).pipe(
@@ -23,6 +23,4 @@ export class TripsService {
 			catchError(error => this.errorService.handleError(error, "getTrips"))
 		);
 	}
-
-
 }
